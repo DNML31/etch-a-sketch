1104b7bf68f0926589298a440d5d741cdeff42a2
Daniel Chun <danielchun31@gmail.com>
2022-08-04 10:45:16 -0700

    makeRow and makeGrid functions added but needs adjusting

--- javascript.js
+++ javascript.js
@@ -1,13 +1,51 @@
+const row = document.querySelector('.container');
+const linebreak = document.createElement('br');
+
+function makeRow (num) {
 
 
-function makeRow () {
-
-    for(i = 0; i < 11; i++) {
-        const grid = document.querySelector('.grid');
+    for(i = 0; i < num; i++) {
         const myRow = document.createElement('div');
-        grid.classList.add('.grid');
-        grid.appendChild(myRow);
+        myRow.classList.add('gridblock');
+        row.appendChild(myRow);
     }
+
 }
 
-makeRow();
+function makeGrid (num) {
+    
+    makeRow(3);
+    row.appendChild(linebreak);
+        // const column = document.querySelector('.container');
+        // column.appendChild(linebreak);
+        makeRow(3);
+        
+        
+        // const myCol = document.createElement('div');
+        // myCol.classList.add('gridblock');
+        // myCol.textContent = makeRow (2);
+        // column.appendChild(myCol);
+
+}
+
+makeGrid(3);
+
+// function makeGrid (num) {
+//     for(j = 0; j < num; j++) {
+        
+//         makeRow(num);
+
+//         const line = document.querySelector('.gridrow');
+//         const grid = document.createElement('div');
+
+//         for(k = 0; k < num; k++){
+//             makeRow(num);
+//         }
+
+//         grid.classList.add('grid');
+//         line.appendChild(grid);
+//     }
+
+// }
+
+// makeGrid(3);
