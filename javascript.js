53d64932ba11b093f32ce831566feb772c2c7d8c
Daniel Chun <danielchun31@gmail.com>
2022-08-05 11:04:59 -0700

    Simplified solution
    The makeGrid function now only takes one parameter for how many
    squares the grid will be made of. The container has a flex wrap to
    make sure it creates the "rows" and "columns" I was trying to achieve
    previously.

--- javascript.js
+++ javascript.js
@@ -1,38 +1,12 @@
 
-const linebreak = document.createElement('br');
-const theGrid = document.querySelector('.container');
-const theGridChild = document.querySelector('.grid');
-
-function makeRow (x) {
+function makeGrid (x) {
+    
     for(i = 0; i < x; i++) {
+        const theGrid = document.querySelector('.container');
         const myRow = document.createElement('div');
-        myRow.classList.add('gridblock'); //this class is red blocks
+        myRow.classList.add('block'); 
         theGrid.append(myRow);
-        // theGrid.append(linebreak);
     }
 }
 
-
-function makeGrid (x, y) {
-    for(j = 0; j < y; j++) {
-        makeRow(x);
-        const myCol = document.createElement('div');
-        myCol.classList.add('gridrock'); //this class is blue blocks
-        theGridChild.append(myCol);
-    }
-}
-makeGrid(4, 4);
-
-
-
-// function makeGrid (x, y) {
-//     for(j = 0; j < y; j++) {
-//         makeRow(x);
-//         const myGrid = document.createElement('div');
-//         myGrid.classList.add('gridblock');
-//         theGrid.append(myGrid);
-
-//     }
-// }
-
-// makeGrid(2, 2);
+makeGrid(256); 
