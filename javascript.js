68a7e3c5f3ea029a34718a4d947b545bbacfad3b
Daniel Chun <danielchun31@gmail.com>
2022-08-05 17:26:28 -0700

    Created btnPress function
    Function to prompt user for the number of blocks in the grid does not
    function properly.
    
    Instead of removing all divs from "container", it is adding them on.
    
    The makeGrid function may need to be re-worked to properly create a
    square grid based on the number from the user.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let theGrid;
+let block;
 
 
 function makeGrid (x) {
@@ -11,10 +13,22 @@ function makeGrid (x) {
             block.classList.add('change');
         });
     }
+}
+makeGrid(256);
 
+function btnPress () {
+
+    let container = document.getElementsByClassName('container');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', () => {
+        num = prompt("How big do you want the grid?");
+        makeGrid(num);
+
+    });
 
 }
-
-makeGrid(256); 
-
-
+btnPress();
