0a0a9e0824d6807b88fe9b67c527440ae52da003
Daniel Chun <danielchun31@gmail.com>
2022-08-15 17:44:20 -0700

    Added an if condition to make sure user selection is not greater than 100

--- javascript.js
+++ javascript.js
@@ -8,15 +8,22 @@ function makeGrid () {
 
     if (typeof y === "number") {
 
-        for(i = 0; i < y; i++) {
+        if (x > 100) {
+            let x = prompt("please choose a number smaller than 100.")
+            let y = x * x;
+            makeGrid(y);
+        } else {
+            for(i = 0; i < y; i++) {
 
-            const theGrid = document.querySelector('#container');
-            const block = document.createElement('div');
-            theGrid.append(block);
-            block.addEventListener('mouseenter', ()=> {
-                block.classList.add('change');
-            });
+                const theGrid = document.querySelector('#container');
+                const block = document.createElement('div');
+                theGrid.append(block);
+                block.addEventListener('mouseenter', ()=> {
+                    block.classList.add('change');
+                });
+            }
         }
+        
     } else if (typeof y !== "number") {
         let x = prompt("please enter a number. how many blocks per side?");
         let y = x * x;
